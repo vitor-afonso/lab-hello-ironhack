b64692cc911c12fd9ef9d6475d89cc99b47b101f
Vitor Afonso <vittokm@hotmail.com>
2022-03-10 21:00:01 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+// jshint esversion:8
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+
+import menuTop from './images/menu-top.svg';
+import ironhackLogo from './images/ironhack-logo.svg';
+import reactLogo from './images/react-logo.svg';
+import icon1 from './images/icon1.png';
+import icon2 from './images/icon2.png';
+import icon3 from './images/icon3.png';
+import icon4 from './images/icon4.png';
+
+const imageMenuTop = <img src={menuTop} alt="menu top" />;
+const logo = <img src={ironhackLogo} alt="logo" />;
+const iconOne = <img src={icon1} alt="icon declarative" />;
+const iconTwo = <img src={icon2} alt="icon components" />;
+const iconThree = <img src={icon3} alt="icon single-way" />;
+const iconFour = <img src={icon4} alt="icon jsx" />;
+
+const App = () => {
+
+    return (
+        <div className="App">
+
+            <section id='top'>
+                <nav id='navbar'>
+                    {logo}
+                    {imageMenuTop}
+                </nav>
+
+                <h1>Say hello to <br/>ReactJS</h1>
+
+                <p>You will learn how to use<br/> 
+                the most popular frontend library,<br/> 
+                and become a super Ninja developer.</p>
+            
+                <button>Awesome!</button>
+            </section>
+
+            <section id='down'>
+                <div>
+                    {iconOne}
+                    <h2>Declarative</h2>
+                    <p>React makes it <br/>
+                    painless to create <br/>
+                    interactive Uls.</p>
+                </div>
+                <div>
+                    {iconTwo}
+                    <h2>Components</h2>
+                    <p>Build encapsulated <br/>components that <br/>manage their state.</p>
+                </div>
+                <div>
+                    {iconThree}
+                    <h2>Single-Way</h2>
+                    <p>A set of immutable <br/>values are passed to <br/>
+                    the component's.</p>
+                </div>
+                <div>
+                    {iconFour}
+                    <h2>JSX</h2>
+                    <p>Statically-typed, <br/>
+                    designed to run on <br/>
+                    modern browsers</p>
+                </div>
+            </section>
+        </div>
+    );
+
+};
 
 export default App;
